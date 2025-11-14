a12a8eab3973026ac267ca438db7eea52df96847
edaild <ch5183sb@gmail.com>
2025-11-14 10:52:30 +0900

    [Feat] 고정형 데이터 서버와 DB 연결 완료

--- Node/Server.js
+++ Node/Server.js
@@ -8,11 +8,37 @@ const pool = mysql.createPool({
     host : 'localhost',
     name : 'root',
     password : '1234',
-    database : 'Metropolis'
+    database : 'metropolis'
 
 });
 
 app.use(express.json());
+
+app.post('/charactercard', async(req, res) =>{
+    try
+    {
+        const[charactercard] = await pool.query(
+            "SELECT character_name, character_description FROM charactercard",
+        );
+    }
+    catch
+    {
+        res.status(500).json({success : false , message : error.message});
+    }
+});
+
+app.post('shop', async(req, res) =>{
+    try
+    {
+         const[shop] = await pool.query(
+            "SELECT c.character_name as character_name,  character_price, charactercard_count FROM shop s JOIN characterCard c on s.charactgercard_id = c.character_id",
+        );
+    }
+    catch
+    {
+         res.status(500).json({success : false , message : error.message});
+    }
+})
 
 app.get('/', (req,res)=>{
     res.send("root 경로에 서버가 성공적으로 연결되 있습니다.");
